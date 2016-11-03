6d2c6c3cf96cb9d985b9275a05520a323f77f5cd
JOOOJ <jjjrachel@hotmail.com>
2016-11-03 10:17:45 +0800

    SqlDataToCSV project with DataTableToCSV functions

---
+++ SqlDataToCSV/DataTableToCSV.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace SqlDataToCSV
+{
+    public class DataTableToCSV
+    {
+        public void Convert(DataTable table,string csvFilePath)
+        {
+            using (StreamWriter writer = new StreamWriter(csvFilePath))
+            {
+                List<string> sb = new List<string>();
+                foreach (DataColumn col in table.Columns)
+                {
+                    string value = PrepareValue(col.ColumnName);
+                    sb.Add(value);
+                }
+                string line = string.Join(",", sb);
+                writer.WriteLine(line);
+            }
+
+            using (StreamWriter writer = new StreamWriter(csvFilePath, true))
+            {
+                List<string> sb = new List<string>(table.Columns.Count);
+                foreach (DataRow row in table.Rows)
+                {
+                    sb.Clear();
+                    foreach (DataColumn col in table.Columns)
+                    {
+                        string value = PrepareValue(row[col].ToString());
+                        sb.Add(value);
+                    }
+                    string line = string.Join(",", sb);
+                    writer.WriteLine(line);
+                }
+
+            }
+        }
+
+        private string PrepareValue(string value)
+        {
+            string result = value.Replace("\"", "\"\"");
+            result = "\"" + result + "\"";
+            return result;
+
+        }
+    }
+}
